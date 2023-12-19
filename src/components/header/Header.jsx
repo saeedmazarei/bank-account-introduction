3414f930f6b99a08eb56401e71d8ef7bd3420a32
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-19 23:22:36 +0330

    Refactor icon with image icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,3 @@
-import { IoIosNotificationsOutline } from 'react-icons/io'
-import { IoTabletPortraitOutline } from 'react-icons/io5'
-import { GoClock } from 'react-icons/go'
-import { CiCalendarDate } from 'react-icons/ci'
-import { AiOutlineLineChart } from 'react-icons/ai'
-import { IoIosArrowDown } from 'react-icons/io'
-
 import Card from '../shared/card/Card'
 import styles from './Header.module.scss'
 import IconText from './icon-text/IconText'
@@ -14,56 +7,38 @@ function Header() {
         <header className={styles['header-container']}>
             <Card className={`${styles.card} flex space-between ver-center`}>
                 <div className={`${styles['time-and-date-container']} flex space-between`}>
+                    <IconText icon="Time-Circle.svg" text="ساعت ۱۳:۳۱" />
                     <IconText
-                        icon={
-                            <GoClock
-                                className="icon-color"
-                                style={{ height: '100%', width: '100%' }}
-                            />
-                        }
-                        text="ساعت ۱۳:۳۱"
-                    />
-                    <IconText
-                        icon={
-                            <CiCalendarDate
-                                className="icon-color"
-                                style={{ height: '100%', width: '100%' }}
-                            />
-                        }
+                        icon="Calendar.svg"
                         text="پنجشنبه ۲۷ مرداد ۱۴۰۲"
                     />
                 </div>
-                <div
-                    className={`${styles['image-icons-filters-container']} flex ver-center`}
-                >
+                <div className={`${styles['image-icons-filters-container']} flex ver-center`}>
                     <div className={`${styles['filters-container']} flex space-around ver-center`}>
                         <IconText
-                            icon={
-                                <AiOutlineLineChart
-                                    className="icon-color"
-                                    style={{ height: '100%', width: '100%' }}
-                                />
-                            }
+                            icon="chart.svg"
                             text="سالی مالی ۱۴۰۲"
                         />
-                        <IoIosArrowDown />
+                        <img src='/assets/images/Arrow---Down-2.svg' />
                     </div>
                     <div className={`${styles['header-icons-container']} flex space-between`}>
                         <div className={`${styles['icon-container']} flex ver-center`}>
-                            <IoTabletPortraitOutline
+                            <img src='/assets/images/geo.svg'
                                 className="icon-color"
                                 style={{ height: '100%', width: '100%' }}
                             />
                         </div>
                         <div className={`${styles['icon-container']} flex ver-center`}>
-                            <IoIosNotificationsOutline
+                            <img src='/assets/images/notif.svg'
                                 className="icon-color"
                                 style={{ height: '100%', width: '100%' }}
                             />
                         </div>
                     </div>
-                    <div className={`${styles['profile-picture-container']} flex hor-center ver-center`}>
-                        <img src='/assets/images/profile.jpeg' />
+                    <div
+                        className={`${styles['profile-picture-container']} flex hor-center ver-center`}
+                    >
+                        <img src="/assets/images/profile.jpeg" />
                     </div>
                 </div>
             </Card>
--- src/components/header/icon-text/IconText.jsx
+++ src/components/header/icon-text/IconText.jsx
@@ -5,14 +5,16 @@ import styles from './IconText.module.scss'
 function IconText({ icon, text }) {
     return (
         <div className={`${styles['icon-text-container']} flex ver-center`}>
-            <div className={`${styles["icon-container"]} flex ver-center`}>{icon}</div>
+            <div className={`${styles['icon-container']} flex ver-center`}>
+                <img src={`/assets/images/${icon}`} className='icon-color' />
+            </div>
             <span style={{ paddingRight: '10%' }}>{text}</span>
         </div>
     )
 }
 
 IconText.propTypes = {
-    icon: PropTypes.element,
+    icon: PropTypes.string,
     text: PropTypes.string,
 }
 
