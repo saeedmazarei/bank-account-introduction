3e31c8960f41707ee736239f8c42804277764ec8
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-19 18:36:58 +0330

    Add App component and config routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { lazy } from 'react'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Layout from './components/Layout'
+const Home = lazy(() => import('./pages/Home'))
+import PathConstants from './routes/pathConstants'
+const BankAccountDefinition = lazy(() => import('./pages/BankAccountDefinition'))
+const Page404 = lazy(() => import('./components/Page404'))
 import './App.css'
 
 function App() {
-
-  return (
-    <>
-      start
-    </>
-  )
+    const router = createBrowserRouter([
+        {
+            // parent route component
+            element: <Layout />,
+            // your custom routing error component
+            errorElement: <Page404 />,
+            // child route components
+            children: [
+                {
+                    path: PathConstants.HOME,
+                    element: <Home />,
+                },
+                // other pages....
+                {
+                    path: PathConstants.BANKACCOUNTDEINITION,
+                    element: <BankAccountDefinition />,
+                },
+            ],
+        },
+    ])
+    return <RouterProvider router={router} />
 }
 
 export default App
--- src/components/navBar/Index.jsx
+++ src/components/navBar/Index.jsx
@@ -1,5 +1,3 @@
-import styles from './Index.module.scss'
-
 function Index() {}
 
 export default Index
