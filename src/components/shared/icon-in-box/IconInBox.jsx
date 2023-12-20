75129eadb08f683a2d269a067d09b47567d200a2
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-20 21:30:12 +0330

    Add onClick event listener to IconBox component

--- src/components/shared/icon-in-box/IconInBox.jsx
+++ src/components/shared/icon-in-box/IconInBox.jsx
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types'
 import Card from '../card/Card'
 import styles from './IconInBox.module.scss'
 
-function IconInBox({ icon }) {
+function IconInBox({ icon, onClick }) {
     return (
         <Card className={styles['icon-container']}>
-            <img src={`/assets/images/${icon}.svg`} />
+            <img src={`/assets/images/${icon}.svg`} onClick={onClick} />
         </Card>
     )
 }
 
 IconInBox.propTypes = {
     icon: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default IconInBox
